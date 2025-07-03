98560c468fb473455b8a0d937100a36a2e7746c5
Az1zbekx <kalilinux42331@gmail.com>
2025-07-03 21:00:41 +0500

    lesson-1

---
+++ lesson-1/1.py
@@ -0,0 +1,6 @@
+name = input("Salom MAAB ga xush kelibsiz\nIsmingiz nima: ")
+age = int(input("Yoshingiz nechida: "))
+if age >= 18:
+    print(f"{name} sizning yoshingiz {age} bo'lgani uchun bizda ta'lim olishingiz mumkin")
+else:
+    print(f"{name} sizning yoshingiz 18 da kichik bo'lgani sababli bizda ta'lim olishingiz mumkin emas")
